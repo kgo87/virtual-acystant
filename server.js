f630319fccbd20679dc93bfc9327e969ee18dc42
Kat Gomozova <kathy.gomozova@gmail.com>
2021-04-10 16:57:13 -0700

    heroku issues

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/virtualAcystant",
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}
 );
 
 // Start the API server
